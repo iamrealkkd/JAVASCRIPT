b6547a0224abd1ffcd92d07a1a99c12030989b71
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-04-06 19:19:16 +0530

    Arithmetic Operators

---
+++ 3.0_OPERATORS_AND_CONDITIONAL_STATEMENTS/01_0_arithmetic_operators.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 10;
+
+console.log("a =", a, "& b =", b, "\n");
+console.log("a + b =", a + b, "\n");
+console.log("a - b =", a - b, "\n");
+console.log("a * b =", a * b, "\n");
+console.log("a / b =", a / b, "\n");
+console.log("a % b =", a % b, "\n");
+console.log("a ** b =", a ** b);//it takes second line automatically which is written in next line
