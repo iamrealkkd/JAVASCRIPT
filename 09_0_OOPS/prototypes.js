e137164fc622640e3ff27d85d427232f135339c3
iamrealkkd <kkrishna.bga@gmail.com>
2026-05-11 20:24:29 +0530

    Updated Prototype

--- 09_0_OOPS/prototypes.js
+++ 09_0_OOPS/prototypes.js
@@ -7,3 +7,16 @@ let tomhardware = {};
 // console.log(`computer`, computer.__proto__);
 console.log(`lenevo`, lenevo.__proto__);
 console.log(`lenevo`, computer.__proto__);//null because it has not access to it
+
+let geneCar = {
+    tyres:4,
+}
+let tesla = {
+    driver:"AI",
+};
+Object.setPrototypeOf(tesla, geneCar);
+
+console.log(`tesla`, geneCar);
+console.log(`tesla`, tesla);
+console.log(`tesla`, tesla.tyres);
+console.log(`tesla`, Object.getPrototypeOf(tesla));
